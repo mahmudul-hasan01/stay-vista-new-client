f961c9ef5af5d7e7bb393bcd8584a1c121cda6c7
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-03-25 12:45:57 +0600

    if there is no room available then no room available is shown

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Card from "./Card";
 import Container from "../Shared/Container";
 import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/Heading";
 
 const Rooms = () => {
 
@@ -23,9 +24,22 @@ const Rooms = () => {
     }, [category])
 
     return (
-        <div className="pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+        <div>
             {
-                rooms.map(room => <Card key={room._id} room={room}></Card>)
+                rooms && rooms.length > 0 ?
+                    <div className="pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+                        {
+                            rooms.map(room => <Card key={room._id} room={room}></Card>)
+                        }
+                    </div>
+                    :
+                    <div className='flex items-center justify-center min-h-[calc(100vh-300px)]'>
+                        <Heading
+                            center={true}
+                            title='No Rooms Available In This Category!'
+                            subtitle='Please Select Other Categories.'
+                        />
+                    </div>
             }
         </div>
     );
