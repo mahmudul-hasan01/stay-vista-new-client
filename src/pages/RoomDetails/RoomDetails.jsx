36dad680aad0c5ba9a91874a8336fea8a27da375
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-03-26 20:40:54 +0600

    upload image inside imageBB

--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -3,10 +3,13 @@ import Container from "../../components/Shared/Container";
 import { useEffect, useState } from "react";
 import Loader from "../../components/Shared/Loader";
 import { Helmet } from 'react-helmet-async';
+import Header from "../../components/RoomDetails/Header";
+import RoomInfo from "../../components/RoomDetails/RoomInfo";
+import RoomReservation from "../../components/RoomDetails/RoomReservation";
 
 const RoomDetails = () => {
     const { id } = useParams()
-    console.log(id);
+
     const [loading, setLoading] = useState(false)
 
     const [rooms, setRooms] = useState({})
@@ -28,14 +31,23 @@ const RoomDetails = () => {
             <Helmet>
                 <title>{rooms?.title}</title>
             </Helmet>
-            <div className="">
-                {/* Header */}
-                <div className="flex flex-col gap-6"></div>
-                {/* Room Info */}
-                <div className=""></div>
-                {/* Calender */}
-                <div className=""></div>
-            </div>
+            {
+                rooms &&
+                <div className="max-w-screen-lg mx-auto">
+                    {/* Header */}
+                    <div className="flex flex-col gap-6">
+                        <Header room={rooms}></Header>
+                    </div>
+                    {/* Room Info */}
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <RoomInfo room={rooms}></RoomInfo>
+                        <div className='md:col-span-3 order-first md:order-last mb-10'>
+                            {/* RoomReservation */}
+                            <RoomReservation room={rooms} />
+                        </div>
+                    </div>
+                </div>
+            }
         </Container>
     );
 };
